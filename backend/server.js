3292b536796b46c4bd2a01ccb01f89c6eb2908ce
Bhavya Gupta <bhavyagupta275@gmail.com>
2026-07-18 15:18:43 +0530

    main final commit

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,12 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,6 @@ import { toast } from "react-toastify";
 
 const api = axios.create({
   baseURL:import.meta.env.VITE_API_URL,
-  withCredentials:true
 });
 
 api.interceptors.request.use(
