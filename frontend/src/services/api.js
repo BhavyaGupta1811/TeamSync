6f8c7fdd2f04e6e77edddfd91b615280b6251d48
Bhavya Gupta <bhavyagupta275@gmail.com>
2026-07-18 15:10:12 +0530

    final app

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,13 +3,13 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:import.meta.env.VITE_API_URL,
+  withCredentials:true
 });
 
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
-
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
